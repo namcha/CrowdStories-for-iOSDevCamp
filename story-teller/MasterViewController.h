df5b80ffccdade553b2847ff87aa6ed6e841336f
Alan Nuchnoi <alan.nuchnoi@live.com>
2013-07-13 13:13:29 -0700

    Initial Commit

---
+++ story-teller/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  story-teller
+//
+//  Created by me on 7/13/13.
+//  Copyright (c) 2013 namcha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
---
+++ story-teller/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  story-teller
+//
+//  Created by me on 7/13/13.
+//  Copyright (c) 2013 namcha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
